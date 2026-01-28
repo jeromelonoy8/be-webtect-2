4f843b0d932a4dbd70724ece1538d70f9758bf50
Jerome <jeromelonoy8@gmail.com>
2026-01-28 13:33:37 +0800

    Activity 2 submission

--- src/students/students.route.ts
+++ src/students/students.route.ts
@@ -1,9 +1,10 @@
 import { Hono } from 'hono';
-import { createStudent, getStudents, updateStudent, deleteStudent } from './students.controller.js';
+import { createStudent, getStudents, updateStudent, deleteStudent,  getStudentById} from './students.controller.js';
 
 const studentsRoute = new Hono();
 
 studentsRoute.get('/', getStudents);
+studentsRoute.get('/:id', getStudentById);
 studentsRoute.post('/', createStudent);
 studentsRoute.put('/:id', updateStudent);
 studentsRoute.delete('/:id', deleteStudent);
