d7e87dd5db87ac73a3d208c573ca50c946413ff6
Jerome <jeromelonoy8@gmail.com>
2026-02-12 12:05:48 +0800

    Midterm Exam

---
+++ src/data/students.data.ts
@@ -0,0 +1,15 @@
+export const studentsData =[
+
+    {
+        id: "1",
+        first_name: "Jerome",
+        last_name: "Yu",
+        email: "jeromelonoy@gmail.com",
+        age: 23,
+        course:"Information Technology",
+        year_level:2,   
+        gpa: 1.9,
+        enrollment: "active",
+        created_at: "2026-1-21",
+    },
+];
